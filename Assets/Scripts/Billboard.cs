8c68d58abc51dd24d15a2f48deb043ad2152f08a
thehugeguy20 <143875940+thehugeguy20@users.noreply.github.com>
2026-03-13 16:01:19 +0000

    setup project with assets/extensions/etc

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    [SerializeField] private BillboardType billboardType;
+
+    public enum BillboardType {LookAtCamera, CameraForward};
+    
+
+    void LateUpdate()
+    {
+        switch (billboardType)
+        {
+            case BillboardType.LookAtCamera:
+                transform.LookAt(Camera.main.transform.position, Vector3.up);
+                break;
+            case BillboardType.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            default:
+                break;
+        }
+    }
+}
